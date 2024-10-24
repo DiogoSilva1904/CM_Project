9948c914184c5a2456f80c7c9ef22415b7b65a09
Miguel <miguelcruzeiro70@gmail.com>
2024-10-24 16:37:28 +0100

    getting steps, calories and km in the watch

--- fitpulseWear/lib/main.dart
+++ fitpulseWear/lib/main.dart
@@ -3,8 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
-import 'package:permission_handler/permission_handler.dart';  // Import permission handler
+import 'package:permission_handler/permission_handler.dart';
 import 'package:workout/workout.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,20 +31,58 @@ class HeartRateMonitor extends StatefulWidget {
   _HeartRateMonitorState createState() => _HeartRateMonitorState();
 }
 
-class _HeartRateMonitorState extends State<HeartRateMonitor> {
+class _HeartRateMonitorState extends State<HeartRateMonitor> with WidgetsBindingObserver {
   final workout = Workout();
   double heartRate = 0;
+  int totalSteps = 0; // Total step count
+  int sessionSteps = 0; // Steps in the current session
+  double totalCalories = 0; // Total calories
+  double sessionCalories = 0; // Calories burned in the current session
+  double totalDistance = 0; // Total distance
+  double sessionDistance = 0; // Distance covered in the current session
   late MqttServerClient client;
+  late Timer workoutTimer;
 
   @override
   void initState() {
     super.initState();
-    requestPermissions(); // Call permissions request here
+    WidgetsBinding.instance.addObserver(this); // Observe app lifecycle
+    requestPermissions();
     initializeMqttClient();
+    loadSavedData(); // Load saved steps, calories, and distance from SharedPreferences
     startWorkoutListener();
   }
 
-  // Request required permissions
+  // Load saved data from SharedPreferences
+  Future<void> loadSavedData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      totalSteps = prefs.getInt('totalSteps') ?? 0;
+      totalCalories = prefs.getDouble('totalCalories') ?? 0.0;
+      totalDistance = prefs.getDouble('totalDistance') ?? 0.0;
+    });
+  }
+
+  // Save current data to SharedPreferences
+  Future<void> saveData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('totalSteps', totalSteps + sessionSteps);
+    await prefs.setDouble('totalCalories', totalCalories + sessionCalories);
+    await prefs.setDouble('totalDistance', totalDistance + sessionDistance);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      saveData(); // Save data when the app is paused
+      stopWorkoutTracking();
+    } else if (state == AppLifecycleState.resumed) {
+      loadSavedData(); // Load data and restart tracking when app is resumed
+      initializeMqttClient();
+      startWorkoutListener();
+    }
+  }
+
   Future<void> requestPermissions() async {
     var sensorStatus = await Permission.sensors.status;
     if (!sensorStatus.isGranted) {
@@ -68,8 +107,6 @@ class _HeartRateMonitorState extends State<HeartRateMonitor> {
     try {
       await client.connect();
       print('Connected to the MQTT broker');
-      //send a message to the broker
-      
     } catch (e) {
       print('Exception: $e');
       client.disconnect();
@@ -80,23 +117,34 @@ class _HeartRateMonitorState extends State<HeartRateMonitor> {
     workout
         .start(
       exerciseType: ExerciseType.walking,
-      features: [WorkoutFeature.heartRate],
+      features: [
+        WorkoutFeature.heartRate,
+        WorkoutFeature.steps,
+        WorkoutFeature.calories,
+        WorkoutFeature.distance,
+      ],
       enableGps: false,
     )
         .then((result) {
       if (result.unsupportedFeatures.isEmpty) {
         workout.stream.listen((event) {
-          if (event.feature == WorkoutFeature.heartRate) {
-            setState(() {
+          setState(() {
+            if (event.feature == WorkoutFeature.heartRate) {
               heartRate = event.value;
-            });
-          }
+            } else if (event.feature == WorkoutFeature.steps) {
+              sessionSteps = event.value.toInt();
+            } else if (event.feature == WorkoutFeature.calories) {
+              sessionCalories = event.value; // Assuming this value is in calories
+            } else if (event.feature == WorkoutFeature.distance) {
+              sessionDistance = event.value; // Assuming this value is in meters
+            }
+          });
         });
 
-        // Start a timer to send heart rate data every 5 seconds
-        Timer.periodic(const Duration(seconds: 5), (Timer timer) {
-          publishHeartRate();
-          print('Heart rate data sent via MQTT');
+        // Start a timer to send data every 5 seconds
+        workoutTimer = Timer.periodic(const Duration(seconds: 5), (Timer timer) {
+          publishWorkoutData();
+          print('Workout data sent via MQTT');
         });
       }
     }).catchError((error) {
@@ -104,40 +152,68 @@ class _HeartRateMonitorState extends State<HeartRateMonitor> {
     });
   }
 
-  void publishHeartRate() {
-    // Create a JSON object with the heart rate data
-    final Map<String, dynamic> heartRateData = {
+  void publishWorkoutData() {
+    final Map<String, dynamic> workoutData = {
       'heartRate': heartRate.round(),
+      'steps': totalSteps + sessionSteps,
+      'calories': totalCalories + sessionCalories,
+      'distance': totalDistance + sessionDistance, // Convert to kilometers if needed
       'timestamp': DateTime.now().toIso8601String(),
     };
 
-    // Convert the map to a JSON string
-    final String heartRateJson = jsonEncode(heartRateData);
+    final String workoutJson = jsonEncode(workoutData);
 
-    // Build the payload
     final builder = MqttClientPayloadBuilder();
-    builder.addString(heartRateJson);
+    builder.addString(workoutJson);
 
-    // Publish the message
     client.publishMessage('sensor/data', MqttQos.atLeastOnce, builder.payload!);
-    print('Heart rate data sent via MQTT: $heartRateJson');
+    print('Workout data sent via MQTT: $workoutJson');
+  }
+
+  void stopWorkoutTracking() {
+    workout.stop();
+    workoutTimer.cancel();
+    print('Workout tracking stopped');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Heart Rate: ${heartRate.round()} bpm',
-          style: const TextStyle(fontSize: 14, color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Heart Rate: ${heartRate.round()} bpm',
+              style: const TextStyle(fontSize: 14, color: Colors.white),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Total Steps: ${totalSteps + sessionSteps}',
+              style: const TextStyle(fontSize: 14, color: Colors.white),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Total Calories: ${(totalCalories + sessionCalories).toStringAsFixed(2)} kcal', // Display total calories with 2 decimals
+              style: const TextStyle(fontSize: 14, color: Colors.white),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Total Distance: ${((totalDistance + sessionDistance) / 1000).toStringAsFixed(2)} km', // Display total distance in kilometers with 2 decimals
+              style: const TextStyle(fontSize: 14, color: Colors.white),
+            ),
+          ],
         ),
       ),
     );
   }
 
+
   @override
   void dispose() {
-    workout.stop();
+    WidgetsBinding.instance.removeObserver(this);
+    saveData(); // Save data when the widget is disposed
+    stopWorkoutTracking();
     client.disconnect();
     super.dispose();
   }
